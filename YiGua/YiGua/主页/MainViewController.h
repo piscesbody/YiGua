0b5415cba71f7b2910a706390616f5f0774099ef
陈宁 <200520656@qq.com>
2017-04-22 17:06:03 +0800

    MainVC 创建

---
+++ YiGua/YiGua/主页/MainViewController.h
@@ -0,0 +1,15 @@
+//
+//  MainViewController.h
+//  YiGua
+//
+//  Created by 陈宁 on 2017/4/22.
+//  Copyright © 2017年 陈宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+- (IBAction)btnClick:(UIButton *)sender;
+
+
+@end
---
+++ YiGua/YiGua/主页/MainViewController.m
@@ -0,0 +1,56 @@
+//
+//  MainViewController.m
+//  YiGua
+//
+//  Created by 陈宁 on 2017/4/22.
+//  Copyright © 2017年 陈宁. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    UIImageView *imageView1 = [[UIImageView alloc]initWithFrame:self.view.bounds];
+    imageView1.image = [UIImage imageNamed:@"fo"];
+    [self.view addSubview:imageView1];
+    
+    UIImageView *imageView2 = [[UIImageView alloc]initWithFrame:self.view.bounds];
+    imageView2.image = [UIImage imageNamed:@"baguantu.jpg"];
+    [self.view addSubview:imageView2];
+    
+    [UIView animateWithDuration:2 animations:^{
+        imageView2.alpha = 0;
+    } completion:^(BOOL finished) {
+        [imageView2 removeFromSuperview];
+        [imageView1 removeFromSuperview];
+        [UIView transitionWithView:self.view duration:1 options:UIViewAnimationOptionTransitionFlipFromLeft animations:nil completion:nil];
+    }];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)btnClick:(UIButton *)sender {
+    
+    
+}
+@end
